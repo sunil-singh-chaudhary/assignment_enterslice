9a94ef142ff0e7c23449647075331c3cd9658714
sunil_singh_chaudhary <chaudharysunil62@gmail.com>
2023-10-11 13:07:19 +0530

    added event for every 2 second update flutter

---
+++ android/app/src/main/kotlin/com/example/assignment_enterslice/LocationStreamHandler/LocationStreamHandlers.kt
@@ -0,0 +1,4 @@
+package com.example.assignment_enterslice.LocationStreamHandler
+
+class LocationStreamHandlers {
+}
---
+++ android/app/src/main/kotlin/com/example/assignment_enterslice/dbHelper/DbHandler.kt
@@ -0,0 +1,24 @@
+package com.example.assignment_enterslice.dbHelper
+
+import android.content.Context
+
+class DbContants {
+
+    fun fetchDataFromDatabase(context : Context): List<Map<String, Any?>> {
+        val dbHelper = LocationDatabaseHelper(context)
+
+        // Read data from the database
+        val data = dbHelper.readAllData() // Implement this function in your dbHelper
+
+        // Convert data to a format that can be sent to Flutter
+        val dataToSend = data.map { location ->
+            mapOf(
+                "latitude" to location[LocationDatabaseHelper.COLUMN_LATITUDE],
+                "longitude" to location[LocationDatabaseHelper.COLUMN_LONGITUDE],
+                "timestamp" to location[LocationDatabaseHelper.COLUMN_TIMESTAMP]
+            )
+        }
+        return dataToSend
+    }
+
+}
