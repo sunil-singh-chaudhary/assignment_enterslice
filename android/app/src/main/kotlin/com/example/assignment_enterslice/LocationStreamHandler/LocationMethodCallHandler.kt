48b55bfe96deb017ed57f40a8833752445f3d2da
sunil_singh_chaudhary <chaudharysunil62@gmail.com>
2023-10-11 17:38:27 +0530

    saperate class for method and event

--- android/app/src/main/kotlin/com/example/assignment_enterslice/LocationStreamHandler/LocationMethodCallHandler.kt
+++ android/app/src/main/kotlin/com/example/assignment_enterslice/LocationStreamHandler/LocationMethodCallHandler.kt
@@ -1,4 +1,58 @@
 package com.example.assignment_enterslice.LocationStreamHandler
 
-class LocationMethodCallHandler {
+import androidx.core.app.ActivityCompat
+import com.example.assignment_enterslice.MainActivity
+import android.os.Build
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+
+class LocationMethodChannelHandler(private val activity: MainActivity) {
+    private val locationPermissionUtils = activity.locationPermissionUtils
+
+    fun handleMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        when (call.method) {
+            "startLocationService" -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (locationPermissionUtils.checkLocationPermission()) {
+                        activity.startService(activity.service)
+                    } else {
+                        requestLocationPermissions()
+                    }
+                } else {
+                    activity.startService(activity.service)
+                }
+
+                result.success(null)
+            }
+
+            "stopLocationService" -> {
+                activity.stopService(activity.service)
+                result.success(null)
+            }
+
+            "fetchDataFromNative" -> {
+                try {
+                    val lastKnownLocation = activity.dbhandler.fetchDataFromDatabase(activity)
+                    result.success(lastKnownLocation)
+                } catch (e: Exception) {
+                    result.error("FETCH_ERROR", e.localizedMessage, null)
+                }
+            }
+
+            else -> {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun requestLocationPermissions() {
+        ActivityCompat.requestPermissions(
+            activity,
+            arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            10
+        )
+    }
 }
--- android/app/src/main/kotlin/com/example/assignment_enterslice/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/assignment_enterslice/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.example.assignment_enterslice
 
 import android.app.Activity
-import android.app.AlertDialog
-import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
-import android.util.Log
 import androidx.core.app.ActivityCompat
+import com.example.assignment_enterslice.LocationStreamHandler.LocationMethodChannelHandler
 import com.example.assignment_enterslice.LocationStreamHandler.LocationStreamHandlers
 import com.example.assignment_enterslice.dbHelper.DbHandler
 import com.example.assignment_enterslice.locationModel.LocationEvent
@@ -18,7 +14,6 @@ import com.example.assignment_enterslice.locationUtils.LocationPermissionUtils
 import com.example.assignment_enterslice.services.LocationService
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.plugin.common.EventChannel
-import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
@@ -29,77 +24,33 @@ class MainActivity : FlutterActivity() {
         private val METHOD_CHANNEL = "location_service" // Define your method channel name
         private val EVENT_CHANNEL = "EVENT_CHANNEL_SUB" // Define your event channel name
     }
-
     val locationStreamHandler = LocationStreamHandlers()
     val dbhandler = DbHandler()
-    private var service: Intent? = null
+    var service: Intent? = null
     private var lastKnownLocation: LocationEvent? = null
     private var methodChannel: MethodChannel? = null
     private var eventChannel: EventChannel? = null
-    private val locationPermissionUtils by lazy { LocationPermissionUtils(this) }
+    val locationPermissionUtils by lazy { LocationPermissionUtils(this) }
+    private val locationMethodChannelHandler by lazy { LocationMethodChannelHandler(this) }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        service = Intent(this, LocationService::class.java) //Register service
-        if (!EventBus.getDefault().isRegistered(this)) { //Register EventBus
+       // Register Service
+        service = Intent(this, LocationService::class.java)
+        //Register service
+        if (!EventBus.getDefault().isRegistered(this)) {
+            //Register EventBus
             EventBus.getDefault().register(this)
         }
 
-
-        methodChannel = MethodChannel(flutterEngine?.dartExecutor!!.binaryMessenger, METHOD_CHANNEL)
         // Register the MethodChannelHandler
-        methodChannel!!.setMethodCallHandler { call: MethodCall, result: MethodChannel.Result ->
-            when (call.method) {
-
-                "startLocationService" -> {
-                    // Start your location service here
-                    Log.e("ONStart click", "onCreate: ")
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        if (locationPermissionUtils.checkLocationPermission()) {
-                            startService(service)
-                        } else {
-                            requestPermissions(
-                                arrayOf<String>(
-                                    android.Manifest.permission.ACCESS_COARSE_LOCATION,
-                                    android.Manifest.permission.ACCESS_FINE_LOCATION
-                                ),
-                                10
-                            )
-                        }
-                    } else {
-                        startService(service)
-                    }
-
-                    result.success(null)
-                }
-
-                "stopLocationService" -> {
-                    // Stop your location service here
-                    print("Service stopped")
-                    stopService(service)
-                    result.success(null)
-                }
-
-                "fetchDataFromNative" -> {
-                    try {
-                        // Fetch data from the database
-                        val LastKnownLocation = dbhandler.fetchDataFromDatabase(this)
-                        // Send the data to Flutter
-                        result.success(LastKnownLocation)
-                    } catch (e: Exception) {
-                        // Handle exceptions if needed
-                        result.error("FETCH_ERROR", e.localizedMessage, null)
-                    }
-                }
-
-                else -> {
-                    result.notImplemented()
-                }
-            }
+        methodChannel = MethodChannel(flutterEngine?.dartExecutor!!.binaryMessenger, METHOD_CHANNEL)
+        methodChannel!!.setMethodCallHandler { call, result ->
+            locationMethodChannelHandler.handleMethodCall(call, result)
         }
 
+        // Register the EventChannel Hanlder
         eventChannel = EventChannel(flutterEngine!!.dartExecutor.binaryMessenger, EVENT_CHANNEL)
         eventChannel!!.setStreamHandler(locationStreamHandler) //inside oncreate
 
